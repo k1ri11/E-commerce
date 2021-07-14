907fe2c6606fca4e73e638c60ec9bc0cb88657fc
k1ri11 <k_trofimovich_k@mail.ru>
2021-07-14 15:46:22 +1000

    begin recyclerview adapter, done category item

---
+++ app/src/main/java/com/example/e_commerce/adapter/CategoryAdapter.java
@@ -0,0 +1,49 @@
+package com.example.e_commerce.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.e_commerce.R;
+import com.example.e_commerce.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder> {
+
+    Context context;
+    List<Category> categories;
+
+    public CategoryAdapter(Context context, List<Category> categories) {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item,parent, false);
+        return new CategoryViewHolder(categoryItems);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryAdapter.CategoryViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static final class CategoryViewHolder extends RecyclerView.ViewHolder{
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
